ef6b30bcbfa542c1eab138562a2aec0f4856b239
“Alexey <a.chernyaev@dodopizza.com>
2019-11-08 21:46:27 +0300

    Add chapter2 examples

---
+++ RabbitMqInActionCSharp.Chapter2.Producer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using BasicProperties = RabbitMQ.Client.Framing.BasicProperties;
+
+namespace RabbitMqInActionCSharp.Chapter2.Publisher
+{
+    class Program
+    { 
+        private const string ExchangeName = "hello-exchange";
+        
+        static void Main(string[] args)
+        {
+            // default host is localhost
+            var factory = new ConnectionFactory();
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.ExchangeDeclare
+            (
+                ExchangeName,
+                "direct",
+                true,
+                false,
+                new Dictionary<string, object> {{"passive", false}}
+            );
+            
+            var messageProperties = new BasicProperties
+            {
+                ContentType = "text/plain"
+            };
+            var messageBody = Encoding.UTF8.GetBytes(args[0]);
+            
+            channel.BasicPublish
+            (
+                ExchangeName,
+                "hola",
+                messageProperties,
+                messageBody
+            );
+        }
+    }
+}
---
+++ RabbitMqInActionCsharp.Chapter2.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMqInActionCsharp.Chapter2.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
